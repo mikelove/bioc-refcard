bc03b3a734b550035efd97e8df30e73e285a994b
Mike Love <michaelisaiahlove@gmail.com>
2015-04-03 17:41:01 -0400

    adding convert ids

---
+++ convertIDs.R
@@ -0,0 +1,13 @@
+# Simon Anders' useful function
+# also lives in http://bioconductor.org/help/workflows/rnaseqGene/
+convertIDs <- function( ids, from, to, db, ifMultiple=c("putNA", "useFirst")) {
+  stopifnot( inherits( db, "AnnotationDb" ) )
+  ifMultiple <- match.arg( ifMultiple )
+  suppressWarnings( selRes <- AnnotationDbi::select(
+    db, keys=ids, keytype=from, columns=c(from,to) ) )
+  if ( ifMultiple == "putNA" ) {
+    duplicatedIds <- selRes[ duplicated( selRes[,1] ), 1 ]
+    selRes <- selRes[ ! selRes[,1] %in% duplicatedIds, ]
+  }
+  return( selRes[ match( ids, selRes[,1] ), 2 ] )
+}
